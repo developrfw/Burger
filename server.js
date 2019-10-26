536f7862ae83f0e1ea679b526410149ee9db86db
developrfw <developrfw@gmail.com>
2019-10-26 00:10:12 -0600

    server.js designed

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+// requiring dependencies
 const express = require("express")
+const app = express();
+
+// Setting the Port
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+// Parsing application body as JSON
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+// Setting up the handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view-engine", "handlebars");
+
+// Importing routes and access
+const routes = require("./controlers/burgers_controller.js");
+
+app.use(routes);
+
+// starting server
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
